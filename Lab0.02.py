472a4d1cce38ff672e679c636cfff18859cbb655
David Gilpin <dgilpin@bmchs.org>
2022-08-22 08:42:30 -0500

    finished Lab 0.02

--- Lab0.02.py
+++ Lab0.02.py
@@ -28,4 +28,31 @@ and the food attributes.
 
 Test your function with your list of pets.
 '''
+# class that instantiates a pet object
+class Pet():
+    def __init__(self, animal, color, food, noise, name):
+        self.animal = animal
+        self.color = color
+        self.food = food
+        self.noise = noise
+        self.name = name
+
+# function that takes in a list of pets and prints their name and food type
+def pet_info(list_of_pets):
+    for pet in list_of_pets:
+        print(f"{pet.name} eats {pet.food}.")
+
+# instances of the Pet class
+pet1 = Pet('dog', 'black', 'dog chow', 'ruff', 'Allie')
+pet2 = Pet('cat', 'brown', 'salmon', 'meow', "Katie Kat")
+pet3 = Pet('fish', 'silver', 'vegetation', 'bloop bloop', 'Charlie')
+pet4 = Pet('snake', 'copper', 'mice', 'ssssss', 'Legless')
+
+# list of pet objects
+PET_LIST = [pet1, pet2, pet3, pet4]
+
+# call the pet info function to output each pets name an food attributes
+pet_info(PET_LIST)
+
+
 
